e64cbb8832099c6750681350d5b5a5d960ab33dd
marcelopedras-ufvjm <marcelo.pedras@ict.ufvjm.edu.br>
2015-02-10 17:11:30 -0200

    autenticação ldap funcionando

--- ldap_authentication.rb
+++ ldap_authentication.rb
@@ -1,18 +1,19 @@
 require 'rubygems'
 require 'net/ldap'
+require 'pp'
 
 server_ip_address = '192.168.1.17'
-#ldap = Net::LDAP.new :host => server_ip_address,
+# ldap = Net::LDAP.new :host => server_ip_address,
 #                     :port => 389,
-                     # :auth => {
-                     #     :method => :simple,
-                     #     :username => "cn=admin,dc=ict,dc=ufvjm",
-                     #     :password => "xxxxxx"
-                     # }
+#                      :auth => {
+#                          :method => :simple,
+#                          :username => "cn=admin,dc=ict,dc=ufvjm",
+#                          :password => "meu password"
+#                      }
 #
 # filter = Net::LDAP::Filter.eq( "cn", "carol" )
 # treebase = "dc=ict,dc=ufvjm"
-
+#
 # ldap.search( :base => treebase, :filter => filter ) do |entry|
 #   puts "DN: #{entry.dn}"
 #   entry.each do |attribute, values|
@@ -22,27 +23,68 @@ server_ip_address = '192.168.1.17'
 #     end
 #   end
 # end
-
+#
 # p ldap.get_operation_result
 
 
+class MyLdapSearch
+
+  def initialize
+    @base = 'ou=Users,dc=ict,dc=ufvjm'
+    @host = '192.168.1.17'
+    @port = 389
+    @auth = {
+        :method => :simple,
+        :username => "cn=admin,dc=ict,dc=ufvjm",
+        :password => "meu password"
+    }
+    @ldap = Net::LDAP.new :host => @host,
+                                 :port => @port,
+                                 :auth => @auth
+
+  end
+
+def ldap_search(user)
+
+  filter = Net::LDAP::Filter.eq( "cn", user )
 
 
-port = 389
+  @ldap.search( :base => @base, :filter => filter ) do |entry|
+    puts "DN: #{entry.dn}"
+    entry.each do |attribute, values|
+      puts "   #{attribute}:"
+      values.each do |value|
+        puts "      --->#{value}"
+      end
+    end
+  end
+
+  p @ldap.get_operation_result
+end
+
+def ldap_authenticate(user,password)
 
 
-def autentica(user, password)
-  server_ip_address = '192.168.1.17'
-treebase = 'cn=Users,dn=ict,dn=ufvjm'
-  port=389
-Net::LDAP.new :host => server_ip_address,
-              :port => port,
-              :base => treebase,
-              :auth => { :username => user,
-                         :password => password,
-                         :method => :simple }
+  result = @ldap.bind_as(
+    base: @base,
+    filter: "uid=#{user}",
+    password: password
+  )
+
+  if result
+    puts "Authenticated #{result.first.dn}"
+    #puts "Result: #{ldap.get_operation_result.code}"
+    #puts "Message: #{ldap.get_operation_result.message}"
+    puts @ldap.get_operation_result
+    true
+  else
+    puts "Authentication FAILED."
+    false
+  end
+end
 
 end
 
-result= autentica('carolfdas','xxxxxx')
-p result.auth('carol', 'xxxxxxx')
+ldap = MyLdapSearch.new
+#ldap.ldap_authenticate('carol','meu password')
+ldap.ldap_search('carol')
