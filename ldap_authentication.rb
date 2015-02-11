9535bcfd5858edede803b64558557cbdafb60a10
marcelopedras-ufvjm <marcelo.pedras@ict.ufvjm.edu.br>
2015-02-11 15:37:02 -0200

    métodos de busca refatorados adição de buscas por usuario e grupo

--- ldap_authentication.rb
+++ ldap_authentication.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'net/ldap'
 require 'pp'
 
-server_ip_address = '192.168.1.17'
+#server_ip_address = '192.168.1.17'
 # ldap = Net::LDAP.new :host => server_ip_address,
 #                     :port => 389,
 #                      :auth => {
@@ -27,9 +27,10 @@ server_ip_address = '192.168.1.17'
 # p ldap.get_operation_result
 
 
-class MyLdapSearch
+class LdapSearch
 
   def initialize
+
     @base = 'ou=Users,dc=ict,dc=ufvjm'
     @host = '192.168.1.17'
     @port = 389
@@ -41,8 +42,28 @@ class MyLdapSearch
     @ldap = Net::LDAP.new :host => @host,
                           :port => @port,
                           :auth => @auth
-
   end
+
+  def group_filter(group)
+    Net::LDAP::Filter.eq("memberOf", "cn=#{group},ou=Groups,dc=ict,dc=ufvjm") if group
+  end
+
+  def person_filter
+    Net::LDAP::Filter.eq("objectclass","inetOrgPerson")
+  end
+
+  def user_filter(user)
+    Net::LDAP::Filter.eq("uid", user) & person_filter
+  end
+
+  def group_and_user_filter(user,group)
+    g_filter = group_filter group
+    u_filter = user_filter user
+
+    g_filter & u_filter
+  end
+
+
 
   def search(filter)
     @ldap.search(:base => @base, :filter => filter) do |entry|
@@ -54,24 +75,42 @@ class MyLdapSearch
         end
       end
     end
-
-    p @ldap.get_operation_result
+    #p @ldap.get_operation_result
   end
-  def ldap_search_by_user(user)
 
-    filter = Net::LDAP::Filter.eq("cn", user)
+  def search_by_user(user,group = nil)
+
+    if group
+      filter = group_and_user_filter(user,group)
+    else
+      filter = user_filter(user)
+    end
+
     search(filter)
-
-
-
   end
 
-  def ldap_authenticate(user, password)
+  def search_by_group(group)
+    filter =
+        person_filter &
+            group_filter(group) &
+            Net::LDAP::Filter.negate(user_filter('nobody'))
 
+    search(filter)
+  end
+
+  def authenticate(user, password, group = nil)
+
+    g_filter = group_filter group
+
+
+    filter = user_filter user
+    if g_filter
+      filter = filter & g_filter
+    end
 
     result = @ldap.bind_as(
         base: @base,
-        filter: "uid=#{user}",
+        filter: filter,
         password: password
     )
 
@@ -86,21 +125,10 @@ class MyLdapSearch
       false
     end
   end
-
-  def search_by_group(group)
-    filter =
-        Net::LDAP::Filter.eq("objectclass","inetOrgPerson") &
-        Net::LDAP::Filter.eq("memberOf", "cn=#{group},ou=Groups,dc=ict,dc=ufvjm")
-
-
-
-    search(filter)
-
-  end
-
 end
 
-ldap = MyLdapSearch.new
-#ldap.ldap_authenticate('carol','meu_password')
+ldap = LdapSearch.new
+
+ldap.authenticate('marcelopedras','meu_password','ICT_admins_labs')
 #ldap.ldap_search_by_user('carol')
-ldap.search_by_group('alunos')
+#ldap.search_by_group('ICT_admin_labs')
